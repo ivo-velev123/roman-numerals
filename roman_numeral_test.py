d2e65d68d8a5057ec38ac8299d9d14a2c128d414
Peter Leaver <peterleaver@mt-mbp-flfypgxgp3.broadband>
2025-06-09 12:14:50 +0100

    Written test for input of 2

--- roman_numeral_test.py
+++ roman_numeral_test.py
@@ -4,3 +4,5 @@ from roman_numeral import romanumeralise
 class TestStingMethods(unittest.TestCase):
     def test_does_output(self):
         self.assertEqual(romanumeralise(1), "I")
+    def test_ouputs_II(self):
+        self.assertEqual(romanumeralise(2), "II")
